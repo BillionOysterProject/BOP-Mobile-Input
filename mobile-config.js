f7cbb97aa6e6020b06cbcdc12d4eb3e1d0bdc0d4
Andrew Blair <andrew@abcd.ca>
2015-06-03 14:28:49 -0700

    build

--- mobile-config.js
+++ mobile-config.js
@@ -5,7 +5,7 @@
 // the entire section is optional.
 App.info({
   id: 'ca.knowledge.bopinput',
-  version: '0.0.4',
+  version: '0.0.5',
   name: 'BOP Input',
   description: 'Data collection app for BOP',
   author: 'ABCD',
