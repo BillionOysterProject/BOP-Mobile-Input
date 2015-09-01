e8cfc052d87dc4b539af31278cb213ccfc852cfd
Andrew Blair <andrew@abcd.ca>
2015-09-01 11:08:58 -0700

    build

--- mobile-config.js
+++ mobile-config.js
@@ -5,7 +5,7 @@
 // the entire section is optional.
 App.info({
   id: 'ca.visualscience.bopinput',
-  version: '0.1.8',
+  version: '0.1.9',
   name: 'BOP Input',
   description: 'Data collection app for BOP',
   author: 'ABCD',
